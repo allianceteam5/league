67dd7db0b2ceee51c34e790d429afa44948b18b0
yangpengfei <fei525long@163.com>
2015-11-05 23:36:31 +0800

    更新

---
+++ app/src/main/java/com/easemob/chatuidemo/BuildConfig.java
@@ -0,0 +1,13 @@
+package com.easemob.chatuidemo;
+
+/**
+ * Created by pfy on 2015/11/5.
+ */
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+    public static final String APPLICATION_ID = "com.easemob.chatuidemo";
+    public static final String BUILD_TYPE = "debug";
+    public static final String FLAVOR = "";
+    public static final int VERSION_CODE = 22;
+    public static final String VERSION_NAME = "";
+}
