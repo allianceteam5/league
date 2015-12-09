661ee4cc9c26b0ae36f4d794e97df97d71a256ec
yangpengfei <fei525long@163.com>
2015-12-09 23:43:32 +0800

    fix bean

--- app/src/main/java/com/league/bean/AnnouncedTheLatestBean.java
+++ app/src/main/java/com/league/bean/AnnouncedTheLatestBean.java
@@ -5,17 +5,18 @@ package com.league.bean;
  */
 public class AnnouncedTheLatestBean {
 
+
     /**
      * tbk : 0
-     * id : 52
+     * id : 53
      * picture : http://7xoc8r.com2.z0.glb.qiniucdn.com/corns/20071224162158623_2.jpg
      * title : 600金币
-     * version : 52
+     * version : 53
      * needed : 10000
      * remain : 0
-     * created_at : 1449061221
-     * date : 1449061221
-     * end_at : 1449407450
+     * created_at : 1449406850
+     * date : 1449406850
+     * end_at : 1449590523
      * islotteried : 0
      * winneruserid : 0
      * winnerrecordid : 0
@@ -30,20 +31,20 @@ public class AnnouncedTheLatestBean {
     private String id;
     private String picture;
     private String title;
-    private String version;
-    private String needed;
-    private String remain;
-    private String created_at;
-    private String date;
+    //    private String version;
+//    private String needed;
+//    private String remain;
+//    private String created_at;
+//    private String date;
     private String end_at;
-    private String islotteried;
-    private String winneruserid;
-    private String winnerrecordid;
-    private String winnernumber;
-    private String foruser;
-    private String kind;
-    private String worth;
-    private String isgot;
+//    private String islotteried;
+//    private String winneruserid;
+//    private String winnerrecordid;
+//    private String winnernumber;
+//    private String foruser;
+//    private String kind;
+//    private String worth;
+//    private String isgot;
 
     public void setTbk(String tbk) {
         this.tbk = tbk;
@@ -60,62 +61,62 @@ public class AnnouncedTheLatestBean {
     public void setTitle(String title) {
         this.title = title;
     }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    public void setNeeded(String needed) {
-        this.needed = needed;
-    }
-
-    public void setRemain(String remain) {
-        this.remain = remain;
-    }
-
-    public void setCreated_at(String created_at) {
-        this.created_at = created_at;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
+    //
+//    public void setVersion(String version) {
+//        this.version = version;
+//    }
+//
+//    public void setNeeded(String needed) {
+//        this.needed = needed;
+//    }
+//
+//    public void setRemain(String remain) {
+//        this.remain = remain;
+//    }
+//
+//    public void setCreated_at(String created_at) {
+//        this.created_at = created_at;
+//    }
+//
+//    public void setDate(String date) {
+//        this.date = date;
+//    }
+//
     public void setEnd_at(String end_at) {
         this.end_at = end_at;
     }
-
-    public void setIslotteried(String islotteried) {
-        this.islotteried = islotteried;
-    }
-
-    public void setWinneruserid(String winneruserid) {
-        this.winneruserid = winneruserid;
-    }
-
-    public void setWinnerrecordid(String winnerrecordid) {
-        this.winnerrecordid = winnerrecordid;
-    }
-
-    public void setWinnernumber(String winnernumber) {
-        this.winnernumber = winnernumber;
-    }
-
-    public void setForuser(String foruser) {
-        this.foruser = foruser;
-    }
-
-    public void setKind(String kind) {
-        this.kind = kind;
-    }
-
-    public void setWorth(String worth) {
-        this.worth = worth;
-    }
-
-    public void setIsgot(String isgot) {
-        this.isgot = isgot;
-    }
+//
+//    public void setIslotteried(String islotteried) {
+//        this.islotteried = islotteried;
+//    }
+//
+//    public void setWinneruserid(String winneruserid) {
+//        this.winneruserid = winneruserid;
+//    }
+//
+//    public void setWinnerrecordid(String winnerrecordid) {
+//        this.winnerrecordid = winnerrecordid;
+//    }
+//
+//    public void setWinnernumber(String winnernumber) {
+//        this.winnernumber = winnernumber;
+//    }
+//
+//    public void setForuser(String foruser) {
+//        this.foruser = foruser;
+//    }
+//
+//    public void setKind(String kind) {
+//        this.kind = kind;
+//    }
+//
+//    public void setWorth(String worth) {
+//        this.worth = worth;
+//    }
+//
+//    public void setIsgot(String isgot) {
+//        this.isgot = isgot;
+//    }
 
     public String getTbk() {
         return tbk;
@@ -132,60 +133,60 @@ public class AnnouncedTheLatestBean {
     public String getTitle() {
         return title;
     }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public String getNeeded() {
-        return needed;
-    }
-
-    public String getRemain() {
-        return remain;
-    }
-
-    public String getCreated_at() {
-        return created_at;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
+    //
+//    public String getVersion() {
+//        return version;
+//    }
+//
+//    public String getNeeded() {
+//        return needed;
+//    }
+//
+//    public String getRemain() {
+//        return remain;
+//    }
+//
+//    public String getCreated_at() {
+//        return created_at;
+//    }
+//
+//    public String getDate() {
+//        return date;
+//    }
+//
     public String getEnd_at() {
         return end_at;
     }
-
-    public String getIslotteried() {
-        return islotteried;
-    }
-
-    public String getWinneruserid() {
-        return winneruserid;
-    }
-
-    public String getWinnerrecordid() {
-        return winnerrecordid;
-    }
-
-    public String getWinnernumber() {
-        return winnernumber;
-    }
-
-    public String getForuser() {
-        return foruser;
-    }
-
-    public String getKind() {
-        return kind;
-    }
-
-    public String getWorth() {
-        return worth;
-    }
-
-    public String getIsgot() {
-        return isgot;
-    }
+//
+//    public String getIslotteried() {
+//        return islotteried;
+//    }
+//
+//    public String getWinneruserid() {
+//        return winneruserid;
+//    }
+//
+//    public String getWinnerrecordid() {
+//        return winnerrecordid;
+//    }
+//
+//    public String getWinnernumber() {
+//        return winnernumber;
+//    }
+//
+//    public String getForuser() {
+//        return foruser;
+//    }
+//
+//    public String getKind() {
+//        return kind;
+//    }
+//
+//    public String getWorth() {
+//        return worth;
+//    }
+//
+//    public String getIsgot() {
+//        return isgot;
+//    }
 }
