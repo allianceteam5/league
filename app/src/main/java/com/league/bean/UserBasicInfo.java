1e527498d33923160595e414f609d3607c2dea49
yangpengfei <fei525long@163.com>
2015-12-14 09:22:51 +0800

    add

---
+++ app/src/main/java/com/league/bean/UserBasicInfo.java
@@ -0,0 +1,51 @@
+package com.league.bean;
+
+/**
+ * Created by pfy on 2015/12/14.
+ */
+public class UserBasicInfo {
+
+    /**
+     * huanxinid : 4
+     * phone : 2
+     * thumb : http://7xkbeq.com1.z0.glb.clouddn.com/Fo-v4V7aCVADWiMXJm9TW-kx37U2
+     * nickname : 2
+     */
+
+    private String huanxinid;
+    private String phone;
+    private String thumb;
+    private String nickname;
+
+    public void setHuanxinid(String huanxinid) {
+        this.huanxinid = huanxinid;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setThumb(String thumb) {
+        this.thumb = thumb;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getHuanxinid() {
+        return huanxinid;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getThumb() {
+        return thumb;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+}
