7693cb88752f90fa865c3af2ba40af05638fffdd
lg <853381092@qq.com>
2015-12-22 14:42:42 +0800

    fix set code

--- app/src/main/java/com/league/utils/api/IClientUrl.java
+++ app/src/main/java/com/league/utils/api/IClientUrl.java
@@ -86,6 +86,6 @@ public class IClientUrl {
     public static final String signup = testServerUrl + "users/signup";
     public static final String usertocardslist = testServerUrl + "usertocards/list";
     public static final String changepwdbypwd = testServerUrl + "users/changepwdbypwd";
-    public static final String checkpstext = testServerUrl + "users/checkpstext";
+    public static final String checkpstext = testServerUrl + "users/checkcptext";
     public static final String changepwd = testServerUrl + "users/changepwd";
 }
